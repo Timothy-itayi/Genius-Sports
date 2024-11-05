5654ada34894f18b52eeb9db5c10f4924841c336
Timothy <timmytam10@gmail.com>
2024-11-05 14:10:28 +1100

    update nav width

--- genius-sports/src/components/Nav.tsx
+++ genius-sports/src/components/Nav.tsx
@@ -3,13 +3,22 @@ import React from "react";
 
 const Nav: React.FC = () => {
   return (
-    <header className="flex justify-between items-center p-6">
-      <div className="flex items-center space-x-6">
-        <img
-          src="https://placehold.co/50x50"
-          alt="Genius Sports Logo"
-          className="h-10"
-        />
+    <header className=" bg-[#0d1226] w-3/4 mx-auto flex justify-between items-center p-6">
+      <div className="   flex items-center space-x-6">
+      <div className="flex flex-col items-center">
+  <img
+    src="/logos/header-logo.svg"
+    alt="Genius Sports Logo"
+    className="h-10 invert"
+  />
+  <img
+    src='/logos/header-title-logo.svg'
+    alt='Genius Sports'
+    className='h-4 invert'
+  />
+</div>
+
+
         <nav className="flex space-x-6">
           <a href="#" className="text-white">
             Why Genius?
