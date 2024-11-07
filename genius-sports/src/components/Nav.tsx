35c927f76b468c999069d2517c5a29d80c75e833
Timothy <timmytam10@gmail.com>
2024-11-07 20:59:45 +1100

    update nav button  finish nav

--- genius-sports/src/components/Nav.tsx
+++ genius-sports/src/components/Nav.tsx
@@ -1,4 +1,5 @@
 // components/Nav.tsx
+import clsx from "clsx";
 import React from "react";
 
 const Nav: React.FC = () => {
@@ -54,25 +55,136 @@ const Nav: React.FC = () => {
       <span className="absolute bottom-0 left-0 w-full h-1/4 bg-gray-700 transform translate-x-full group-hover:translate-x-0 transition-all duration-1600 ease-out group-hover:bg-[#2FB2EA] delay-[300ms]"></span>
 
 
-  <span className="relative z-10 flex items-center justify-center">
-        <span className="word-container">
-          <span className="flip-letter letter-delay-0">C</span>
-          <span className="flip-letter letter-delay-1">o</span>
-          <span className="flip-letter letter-delay-2">n</span>
-          <span className="flip-letter letter-delay-3">t</span>
-          <span className="flip-letter letter-delay-4">a</span>
-          <span className="flip-letter letter-delay-5">c</span>
-          <span className="flip-letter letter-delay-6">t</span>
-        </span>
-        <span className="word-container">
-          <span className="flip-letter letter-delay-7">S</span>
-          <span className="flip-letter letter-delay-8">a</span>
-          <span className="flip-letter letter-delay-9">l</span>
-          <span className="flip-letter letter-delay-10">e</span>
-          <span className="flip-letter letter-delay-11">s</span>
-        </span>
-      </span>
+   {/* Parent container with relative position */}
+<div className="relative text-center">
+  {/* Top set with higher z-index */}
+  <div className="relative z-30 overflow-hidden text-white">
+    <p className="inline-block">
+      <span className={clsx(
+        "inline-block animate-slide-up",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>C</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[5ms]",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>o</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[10ms]",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>n</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[15ms]",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>t</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[25ms]",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>a</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[40ms]",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>c</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[50ms]",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>t</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[70ms] group-hover: -translate-y-full duration-500 ease-in-out"
+      )}>&nbsp;</span>
+    </p>
+    <p className="inline-block">
+      <span className={clsx(
+        "inline-block animate-slide-up",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>S</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[90ms]",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>a</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[100ms]",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>l</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[150ms]",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>e</span>
+      <span className={clsx(
+        "inline-block animate-slide-up delay-[200ms]",
+        "group-hover:opacity-0 group-hover:-translate-y-full duration-500 ease-in-out"
+      )}>s</span>
+    </p>
+  </div>
+
+  {/* Bottom set positioned below the top set */}
+  <div className={clsx(
+    "absolute text-center",
+    "top-full", // This will position the bottom set below the top set
+    "text-blue-900"
+  )}>
+    <div className=" ">
+      <p className="inline-block">
+        <span className={clsx(
+          "inline-block animate-slide-up",
+          "opacity-0 group-hover:opacity-100 group-hover: -translate-y-full duration-500 ease-in-out"
+        )}>C</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[5ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>o</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[10ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>n</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[15ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>t</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[25ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>a</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[40ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>c</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[50ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>t</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[70ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>&nbsp;</span>
+      </p>
+      <p className="inline-block">
+        <span className={clsx(
+          "inline-block animate-slide-up",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>S</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[90ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>a</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[100ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>l</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[150ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>e</span>
+        <span className={clsx(
+          "inline-block animate-slide-up delay-[200ms]",
+          "opacity-0 group-hover:opacity-100 group-hover:-translate-y-full duration-500 ease-in-out"
+        )}>s</span>
+      </p>
+    </div>
+  </div>
+</div>
+
     </button>
+
     </header>
   );
 };
